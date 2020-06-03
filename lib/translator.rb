fd2edda6b66232d3992ca23656f11f0b0c7ec80e
Robert Defendini <robdefendini@gmail.com>
2020-06-03 18:01:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,9 @@ def load_library(file_path)
     end 
   response
 end
+
 ####
+
 def get_japanese_emoticon(file_path, emoticon)
                                           # code goes here
     response = nil
@@ -39,6 +41,7 @@ def get_english_meaning(file_path, emoticon)
     library = load_library(file_path)
     library["get_meaning"].each do |english, meaning|
     if emoticon == english
+      binding.pry
       response = meaning
     end
   end
