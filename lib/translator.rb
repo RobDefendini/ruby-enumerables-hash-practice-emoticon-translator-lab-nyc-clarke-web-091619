a2b9e44bc313c943e11c122a5d1c3e1d4a42d14e
Robert Defendini <robdefendini@gmail.com>
2020-06-03 17:50:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,50 @@
-# require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+                                          # code goes here  
+    response = { "get_meaning" => {},
+                "get_emoticon" => {}  }                            
+    file = File.read(file_path)
+    emoticons = YAML.load(file)
+    emoticons.each do |trans, emot|
+      response["get_meaning"][emot[1]] = trans
+      response["get_emoticon"][emot[0]] = emot[1]
+    end 
+  response
+end
+####
+def get_japanese_emoticon(file_path, emoticon)
+                                          # code goes here
+    response = nil
+    library = load_library(file_path)
+    
+    library["get_emoticon"].each do |meaning, japanese|
+    if emoticon == meaning
+      response = japanese
+    end 
+  end
+    if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+#  binding.pry
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+                                          # code goes here
+    response = nil
+    library = load_library(file_path)
+    library["get_meaning"].each do |english, japanese|
+    if emoticon == english
+      response = japanese
+    end
+  end
+    if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end 
 end
